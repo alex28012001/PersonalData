d413b734b8119547331ccc114e619d8ca72abd19
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2019-08-16 11:21:33 +0300

    Added structure creating user by html

---
+++ src/DataCollector.Core/Providers/IUserProvider.cs
@@ -0,0 +1,17 @@
+﻿using DataCollector.Models.Entities;
+
+namespace DataCollector.Core.Providers
+{
+    /// <summary>
+    /// The interface contains logic of creating user entity.
+    /// </summary>
+    public interface IUserProvider
+    {
+        /// <summary>
+        /// Create user by data.
+        /// </summary>
+        /// <param name="data">The data, which needed for creating user entity.</param>
+        /// <returns>The user entity.</returns>
+        User CreateUser(string data);
+    }
+}
