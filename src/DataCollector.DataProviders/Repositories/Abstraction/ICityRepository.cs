9044ca9d4dfb7b532bdb9ded8365909cb4cf7318
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2019-08-23 15:28:03 +0300

    Added city entity

---
+++ src/DataCollector.DataProviders/Repositories/Abstraction/ICityRepository.cs
@@ -0,0 +1,10 @@
+﻿using DataCollector.Models.Entities;
+using System.Threading.Tasks;
+
+namespace DataCollector.DataProviders.Repositories.Abstraction
+{
+    public interface ICityRepository
+    {
+        Task<City> GetByCityAsync(string city);
+    }
+}
